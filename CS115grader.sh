2bcb3ea482867ce149ef53477895fc7c6a34a9ef
Daniel C <65579510+DanielCr2020@users.noreply.github.com>
2024-01-10 12:26:27 -0500

    Updated comments at the top of the file

--- CS115grader.sh
+++ CS115grader.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/bash
 # CS115grader.sh
 
-#Daniel Craig
-#CS 115 CA
-#CS 115 bash script grading test
+# Daniel Craig
+# Bash script that automates running a test script against multiple students.
 
-#Runs test script against all files in a folder, saving output to Output.txt
+# Runs test script against all files in a folder, saving the results to Output.txt
 
-#Runs the test script for all files in the specified folder, 
-#modifying the test script file to import the current file
-#then runs the test script for that file, saving the output to Output.txt in the same folder as the test script
-#Output.txt has the output from the test script each file as well as the names of each file. They are numbered as well.
-#For some test scripts, change the import so that only one thing needs to be changed.
-#What I did was take all of my students submissions and put them in their own folder
-#I usually ran this in the top level directory. For me, that was /mnt/c/Users/Daniel/Documents/College Work/Semester 5/CS 115 CA/
-    # (This was on WSL2)
-#I had to have the /bin/ stuff to get it to work for me. You may need to change that.
+# It modifies the import statement in the test script to import the current student's submission.
+# It then runs the test script for that file, saving the output to Output.txt. Output.txt is saved in the same folder as the test script.
+# Output.txt has the output from the test script each file as well as the names of each file. Each student is numbered.
+# For some test script files, you may want to change the import statement so that only one text replacement needs to happen.
+
+# This script uses `python3` as a command. 
 
 if [[ $# -ne 4 ]] 
 then
